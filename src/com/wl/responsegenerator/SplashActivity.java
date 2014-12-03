25386d40e5285cd77f8f2574d7b06ce42b5c1289
gatimus <william.g.lohan@gmail.com>
2014-12-03 14:59:54 -0800

    Splash Screen

--- src/com/wl/responsegenerator/SplashActivity.java
+++ src/com/wl/responsegenerator/SplashActivity.java
@@ -1,13 +1,28 @@
 package com.wl.responsegenerator;
 
 import android.app.Activity;
+import android.content.Intent;
 import android.os.Bundle;
+import android.os.Handler;
 
 public class SplashActivity extends Activity {
+	
+	// Splash screen timer
+    private static final int SPLASH_TIME_OUT = 3_000;
 
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.activity_splash);
-	}
-}
+		new Handler().postDelayed(new Runnable() {
+            @Override
+            public void run() {
+                // This method will be executed once the timer is over
+                Intent i = new Intent(SplashActivity.this, MainActivity.class);
+                startActivity(i);
+                // close this activity
+                finish();
+            } //run()
+        }, SPLASH_TIME_OUT);
+    } //onCreate(Bundle savedInstanceState)
+} //class
